2a9594106f579c9942392bef825ac660e368dec4
Jan Prachař <jan.prachar@gmail.com>
2010-06-17 10:59:36 +0200

    Loader ensures if file exists

--- lib/loader.php
+++ lib/loader.php
@@ -2,6 +2,8 @@
 
 function __autoload($class_name)
 {
-	if (strpos($class_name, 'isqua') === 0)
-		require(__DIR__.str_replace('\\','/',substr($class_name,5).'.php'));
+	if (strpos($class_name, 'isqua') === 0) {
+		$file = __DIR__.str_replace('\\','/',substr($class_name,5).'.php');
+		if (file_exists($file)) require($file);
+	}
 }
