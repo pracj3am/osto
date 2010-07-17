93279abcfa27f4dc09ec121263a55f8828dd52d9
Jan Prachař <jan.prachar@gmail.com>
2010-07-17 19:58:31 +0200

    Typo

--- lib/loader.php
+++ lib/loader.php
@@ -3,7 +3,7 @@
 function __autoload($class_name)
 {
 	if (strpos($class_name, 'osto') === 0) {
-		$file = __DIR__.str_replace('\\','/',substr($class_name,5).'.php');
+		$file = __DIR__.str_replace('\\','/',substr($class_name,4).'.php');
 		if (file_exists($file)) require($file);
 	}
 }
